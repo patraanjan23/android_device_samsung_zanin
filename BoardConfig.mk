9a77b2e5c9d4116ba00467c26d5dd85c2dcbdd9a
patraanjan23 <patraanjan23@gmail.com>
2014-03-10 01:25:24 +0530

    disabled internal sd tweak

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,16 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ########################################################################
 USE_CAMERA_STUB := true
 ########################################################################                                                                      
@@ -25,7 +38,7 @@ BOARD_FLASH_BLOCK_SIZE := 1024  #131072
 TARGET_PREBUILT_KERNEL := device/samsung/zanin/kernel
 #########################################################################
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_SDCARD_INTERNAL := true
+#BOARD_HAS_SDCARD_INTERNAL := true
 BOARD_CUSTOM_GRAPHICS:= ../../../device/samsung/zanin/recovery/graphics.c
 #########################################################################
 
